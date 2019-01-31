4836851b630151dac6b9a75a5f1ddedd779daac0
David Banks <amoebae@gmail.com>
2019-01-31 10:42:05 +0000

    restrict roots

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,10 @@ module.exports = {
     // This is configuration created by "npx ts-jest config:init".
     preset: 'ts-jest',
     testEnvironment: 'node',
+
     
     // This is custom configuration.
+    // Without these, jest will attempt to scan the whole repository.
+    roots: ["<rootDir>/src", "<rootDir>/test"],
     testMatch: ["<rootDir>/test/**/*.ts"]
 };
